41ec5f95bfd347325ca440ec3f8ecb7ef31079bd
macolszewski <olschevski@gmail.com>
2018-08-26 13:28:23 +0200

    Dodano wszsytkie metody i testy

--- src/main/java/tdd/Wydarzenie.java
+++ src/main/java/tdd/Wydarzenie.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Wydarzenie {
     private String tytul;
     private List<Uczestnik> listaUczestnikow;
+    private int liczbaUczestnikow;
 
     Wydarzenie (String tytul) throws WydarzenieNullException {
         if (tytul == null) {
@@ -13,6 +14,15 @@ public class Wydarzenie {
         }
         this.tytul = tytul;
         this.listaUczestnikow = new LinkedList<>();
+        liczbaUczestnikow=5;
+    }
+
+    public int getLiczbaUczestnikow() {
+        return liczbaUczestnikow;
+    }
+
+    public void setLiczbaUczestnikow(int liczbaUczestnikow) {
+        this.liczbaUczestnikow = liczbaUczestnikow;
     }
 
     public String getTytul() {
@@ -21,5 +31,26 @@ public class Wydarzenie {
 
     public List<Uczestnik> getListaUczestnikow() {
         return listaUczestnikow;
+    }
+
+    public void dodajUczestnika(Uczestnik uczestnik) throws UczestnikNullException {
+        if (uczestnik==null) {
+            throw new UczestnikNullException("Uczestnik nie może być nullem");
+        }
+        this.listaUczestnikow.add(uczestnik);
+    }
+
+    public void usunUczestnika(Uczestnik uczestnik) throws UczestnikNullException {
+        if (uczestnik==null) {
+            throw new UczestnikNullException("Uczestnik nie może być nullem");
+        } else {
+            Uczestnik doWywalenia = null;
+            for (Uczestnik uczestnikWydarzenia : listaUczestnikow ) {
+                if (uczestnik.equals(uczestnikWydarzenia)) {
+                    doWywalenia = uczestnikWydarzenia;
+                }
+            }
+            listaUczestnikow.remove(doWywalenia);
+        }
     }
 }
--- src/test/java/tdd/WydarzenieTest.java
+++ src/test/java/tdd/WydarzenieTest.java
@@ -2,12 +2,21 @@ package tdd;
 
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
 public class WydarzenieTest {
+    static Wydarzenie wydarzenie;
+
+
+    @BeforeClass
+    public static void noweWydarzenie() throws WydarzenieNullException {
+         WydarzenieTest.wydarzenie = new Wydarzenie("Testowe");
+    }
 
 
     @Test
@@ -27,5 +36,46 @@ public class WydarzenieTest {
         String tytul = null;
 //        when - then
         Wydarzenie result = new Wydarzenie(tytul);
+    }
+
+    @Test
+    public void czyMetodaDodaniaNowegoUczestnikaDoWydarzeniaDziala() throws UczestnikNullException {
+//        given
+        Uczestnik testowy = new Uczestnik("test","testowy");
+
+//        when
+        WydarzenieTest.wydarzenie.dodajUczestnika(testowy);
+//        then
+        assertThat(WydarzenieTest.wydarzenie.getListaUczestnikow().get(0), equalTo(testowy));
+    }
+
+    @Test(expected = UczestnikNullException.class)
+    public void czyMetodaDodaniaNowegoUczestnikaNullDoWydarzeniaBlad () throws UczestnikNullException {
+//        given
+        Uczestnik testowy = null;
+//        when - then
+        WydarzenieTest.wydarzenie.dodajUczestnika(testowy);
+    }
+
+    @Test
+    public void czyMoznaUstalicIloscOsobWWydarzeniu() {
+//        given
+        int iloscOsob = 10;
+//        when
+        WydarzenieTest.wydarzenie.setLiczbaUczestnikow(iloscOsob);
+//        then
+        assertThat(WydarzenieTest.wydarzenie.getLiczbaUczestnikow(), is(iloscOsob));
+    }
+
+    @Test
+    public void czyMoznaUsunacUczestnikaZListyUczestnikow() throws UczestnikNullException {
+//        given
+        Uczestnik testowy = new Uczestnik("Test","Testowy");
+//        when
+        WydarzenieTest.wydarzenie.dodajUczestnika(testowy);
+        WydarzenieTest.wydarzenie.usunUczestnika(testowy);
+//        then
+        assertThat(wydarzenie.getListaUczestnikow(), not(contains(testowy)));
+
     }
 }
